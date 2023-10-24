0a588d1d4527b78361b0b49e10d21f2b5b9282fe
Awojide Margaret <170806003@live.unilag.edu.ng>
2023-10-24 21:40:05 +0100

    streamlit initialization

--- rayo.py
+++ rayo.py
@@ -1,2 +1,3 @@
 import streamlit as st
-st.title("My very first application")
+st.title("Rayo : Your News Companion")
+st.write("Hello there! My name is Rayo and I will provide you the latest headline news real time from BBC as well as a summary of the news content. Stay Tuned")
