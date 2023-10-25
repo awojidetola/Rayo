bf7aa33e17b1b6834dea67dcd2e40341237f0263
Awojide Margaret <170806003@live.unilag.edu.ng>
2023-10-25 19:13:37 +0100

    changes

--- bbc_news.py
+++ bbc_news.py
@@ -85,16 +85,15 @@ def full_text(url, suppress_st_warning=True):
 
 full_story = full_text(story_link)
 
-model_name = "facebook/bart-large-cnn"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-
 st.button("Clear", type="primary",key=5)
 if st.button('Submit',key=6):
 
     @st.cache
     def generate_summary(input_text):
+        model_name = "facebook/bart-large-cnn"
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
         input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=1024, truncation=True)
         summary_ids = model.generate(input_ids, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
