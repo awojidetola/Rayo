7433fc17c75436c5c61665cd3a512ffa9c730bc8
Margaret Awojide <49078266+awojidetola@users.noreply.github.com>
2023-10-27 09:46:00 +0100

    Update bbc_news.py

--- bbc_news.py
+++ bbc_news.py
@@ -25,7 +25,7 @@ Experience Rayo where the world's news meets your interest, all in one place.
 #Select a news category
 default_url = "https://www.bbc.com"
 
-category = st.selectbox("Select a news category of your choice",("World","Business","Politics","Health","Science & Environment","Technology","Entertainment & Arts"))
+category = st.selectbox("Select a news category of your choice",("World","Business","Politics","Health","Technology","Entertainment & Arts"))
 if category == "World":
     url = "https://www.bbc.com/news/world"
 elif category == "Business":
@@ -34,8 +34,6 @@ elif category == "Politics":
     url = "https://www.bbc.com/news/politics"
 elif category == "Health":
     url = "https://www.bbc.com/news/health"
-elif category == "Science & Environment":
-    url = "https://www.bbc.com/news/science_and_environment"
 elif category == "Technology":
     url = "https://www.bbc.com/news/technology"
 elif category == "Entertainment & Arts":
